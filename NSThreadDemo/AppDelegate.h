6b7fbd97e31518ad1767179f7240e162ea969de1
YangJian <229201093@qq.com>
2016-08-19 10:57:19 +0800

    Initial Commit

---
+++ NSThreadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSThreadDemo
+//
+//  Created by GongHui_YJ on 16/8/19.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSThreadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSThreadDemo
+//
+//  Created by GongHui_YJ on 16/8/19.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
